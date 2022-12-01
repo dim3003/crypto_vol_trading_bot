62ad942ea98c491f1ab9004f82e40e9a894e1872
dim3003 <andre.d3003@gmail.com>
2022-12-01 15:48:10 +0100

    get crypto name 1inch

---
+++ get_crypto_names.py
@@ -0,0 +1,23 @@
+import requests, json
+import pandas as pd
+
+
+
+def get_tokens_1inch(save=0):
+    """
+    Gets cryptocurrencies available on 1inch from tokenlist.org
+    """
+    r = requests.get("https://wispy-bird-88a7.uniswap.workers.dev/?url=http://tokens.1inch.eth.link")
+    r = r.json()
+    token_data = r['tokens']
+    df = pd.DataFrame(token_data)
+    if save == 1:
+        df.to_pickle("token_1inch.pkl")
+    return df
+
+
+get_tokens_1inch(1)
+
+
+
+
