0c860b55091eb6e20fbd879c0a40385323a11cf4
dim3003 <andre.d3003@gmail.com>
2022-12-06 18:36:20 +0100

    test 1inch empty

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import numpy as np
 import get_crypto_names as gcn
 
 class Test(unittest.TestCase):
-    def test_all_cols(self):
+    def test_1inch_colnames(self):
         """
         Test that get crypto names 1inch fetcher has all columns needed
         """
-        col_names = np.array(['address', 'chhainId', 'name', 'symbol', 'decimals', 'logoURI'])
+        col_names = np.array(['address', 'chainId', 'name', 'symbol', 'decimals', 'logoURI'])
         r = gcn.get_tokens_1inch().columns.values
         self.assertEqual(len(col_names), len(r))
         
@@ -15,6 +15,10 @@ class Test(unittest.TestCase):
             if r[i]!=col_names[i]:
                 print(f"{r[i]} != the test column {col_names[i]}")
                 self.assertEqual(0,1,f"The column {r[i]} is not the same as the test column {col_names[i]}")
+
+        if len(r)==0:
+            x = 1
+            self.assertEqual(0,1,"There is nothing in the 1inch dataframe :/") 
 
 
 if __name__ == '__main__':
