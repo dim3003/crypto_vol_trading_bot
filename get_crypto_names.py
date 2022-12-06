ec12a15363e85e351c9a6240ba8052134ebf5f1f
dim3003 <andre.d3003@gmail.com>
2022-12-06 19:05:21 +0100

    df cleaning

--- get_crypto_names.py
+++ get_crypto_names.py
@@ -13,11 +13,12 @@ def get_tokens_1inch(save=0):
     df = pd.DataFrame(token_data)
     if save == 1:
         df.to_pickle("token_1inch.pkl")
+    #some data cleaning...
+    df = df.convert_dtypes()
+    df.dropna(inplace=True)
     return df
 
 
-#get_tokens_1inch(1)
-df = pd.read_pickle("token_1inch.pkl")
-
+df = get_tokens_1inch()
 
 
