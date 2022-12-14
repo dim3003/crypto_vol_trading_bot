91488c15083b6a00fce363e51bd7cf470c98649f
dim3003 <andre.d3003@gmail.com>
2022-12-14 16:03:13 +0100

    historical prices fetched

--- get_historical_prices.py
+++ get_historical_prices.py
@@ -1,3 +1,48 @@
+import pandas as pd
+import numpy as np
+import postgres as pg
+from datetime import date, datetime
+from pycoingecko import CoinGeckoAPI
+
+#set up coingecko API
+cg = CoinGeckoAPI()
+
+#check asset slug for chainId 1 
+assets_cg = cg.get_asset_platforms() 
+df_assets = pd.DataFrame(assets_cg)
+cg_chain_id = df_assets[df_assets.chain_identifier == 1].id.values[0]
 
 
-#use ​/coins​/{id}​/contract​/{contract_address}​/market_chart​/ coingecko endpoint
+#get the data
+df_names = pg.get_postgres()
+
+
+
+
+def get_prices(df_names=df_names, cg_chain_id=cg_chain_id, cg=cg):
+    """ Gets all of the historical price data from coingecko from a json of cryptocurrency on 1inch"""
+
+    #create a df with index starting in 2012 as ts
+    df = pd.DataFrame(index=pd.date_range(start='1/1/2012', end=date.today()))
+    df.index = df.index.values.astype(np.int64) // 10 ** 6
+    
+    
+    for i in range(len(df_names)):
+        print(50*"=")
+        print(df_names.name[i], "coingecko data extraction", i, "/", len(df_names))
+        r = cg.get_coin_market_chart_from_contract_address_by_id(cg_chain_id, df_names.address[0], "USD", 5000)["prices"]
+        df_temp = pd.DataFrame(r)
+        df_temp.set_index(0, inplace=True)
+        df_temp.rename(columns={1: f"{df_names.name[i]}_USD"}, inplace=True)
+        df = df.merge(df_temp, how="left", left_index=True, right_index=True)
+    
+    df.index = pd.to_datetime(df.index, unit='ms')
+    print(40*"-")
+    return df
+
+
+#df = get_prices(df_names)
+#df.to_pickle("df_prices.pkl")
+df = pd.read_pickle("df_prices.pkl")
+df = df.dropna(how="all")
+print(df)
