6ce766b7f6008d5024e251b1bae0162e6f39e250
dim3003 <andre.d3003@gmail.com>
2023-01-03 16:25:55 +0100

    class creation

--- analysis.py
+++ analysis.py
@@ -13,117 +13,134 @@ LIQUIDITY_FEE = 0.003 #used the medium fee tier of uniswap as estimate
 VOL_WINDOW_DAYS = 30 #period for volatility calc 
 MONTHLY_SHOW = 0 #set to one to show monthly returns
 
-def total_returns(returns):
-    """Gives the total returns with a investment of 1 and the number of periods as a tuple"""
-    r = returns + 1
-    pnl = r.product()
-    return (pnl, len(r))
+class Analyzer():
+    def __init__(self, df_price, nbr_days=NBR_DAYS, slippage_fee=SLIPPAGE_FEE, liquidity_fee=LIQUIDITY_FEE, vol_window_days=VOL_WINDOW_DAYS):
+        self.df_price = df_price
+        self.nbr_days = nbr_days
+        self.slippage_fee = slippage_fee
+        self.liquidity_fee = liquidity_fee
+        self.vol_window_days = vol_window_days
+        self.returns = self.get_clean_returns(df_price)
 
-def returns_detailed(returns, weights, weight):
-    """
-    Calculates the returns of a dataframe with the liquidity and slippage fees associated 
-    Gives as second argument a pd series with number of trades
-    """
-    #get turnover of portfolio for the day in percentage
-    weights_calc = weights+1
-    df_turnover = weights_calc.pct_change()
-    df_turnover[df_turnover!=0] = weight
-    df_turnover.iloc[0,:] = 1/len(df_turnover.columns) #sums up to one for turnover
-    #get nbr of trades for gas fees calcs
-    df_nbr_trades = df_turnover.copy()
-    df_nbr_trades[df_nbr_trades != 0] = 1
-    df_nbr_trades = df_nbr_trades.sum(axis=1)
-    #multiply it with (SLIPPAGE + LIQUID FEE) 
-    df_turnover = df_turnover.sum(axis=1)
-    df_fees = df_turnover * (SLIPPAGE_FEE + LIQUIDITY_FEE)
-    df_returns_net = returns - df_fees
-    return df_returns_net, df_nbr_trades
+    def get_clean_returns(self):
+        """ Gives back the cleaned returns with the df price given """
+        df = self.df_price
+        #data cleaning
+        df.fillna(method="ffill", inplace=True) #adds missing days
+        df = df.iloc[len(df)-NBR_DAYS:] #keeps only cryptos with 2 year of data
+        df.dropna(axis=1, inplace=True)
+        #get returns
+        df_returns = df.pct_change()
+        df_returns.dropna(how="all", inplace=True)
+        #change returns outliers to 0
+        val = df_returns.values
+        og_shape = val.shape
+        val = val.ravel()
+        df_temp = pd.Series(val)
+        df_temp[(df_temp > 1) | (df_temp < -1)] = 0
+        df_temp = df_temp.values.reshape(og_shape)
+        df_returns = pd.DataFrame(df_temp, columns = df_returns.columns, index=df_returns.index)
+        return df_returns
 
-def monthly_returns(returns):
-    #get first date and find first day of next month
-    first = returns.index[0]
-    if int(first.strftime("%d")) != 1:
-        next_month = first.replace(day=27) + timedelta(days=5)
-        first = next_month - timedelta(days=(next_month.day-1))
-    #get last date and find last day of last month
-    last = returns.index[-1]
-    last = last - timedelta(days=last.day)
-    #filter df
-    returns = returns[(returns.index >= first) & (returns.index <= last)]
-    #average returns over each month
-    df = pd.DataFrame()
-    while len(returns) > 0:
-        next_month = returns.index[0].replace(day=27) + timedelta(days=5)
-        last_day = next_month - timedelta(days=next_month.day)
-        monthly_returns = returns[returns.index <= last_day] + 1
-        monthly_returns = pd.Series(monthly_returns.product() - 1, index=[last_day.strftime("%y.%m")])
-        df = pd.concat([df,monthly_returns])
-        returns = returns[returns.index > last_day]
-    df.rename(columns={0: "average_returns"}, inplace=True)
-    return df
+    def total_returns(returns):
+        """Gives the total returns with a investment of 1 and the number of periods as a tuple"""
+        r = returns + 1
+        pnl = r.product()
+        return (pnl, len(r))
 
-def sharpe(returns):
-    """
-    Returns the Sharpe ratio of returns given based on the earning rate of the Compound protocol
-    """
-    return round(((total_returns(returns)[0] - 1) - 0.0154) / returns.std(), 4) #0.0154 value is earnings of Compound on 01.01.23
+    def returns_detailed(returns, weights, weight):
+        """
+        Calculates the returns of a dataframe with the liquidity and slippage fees associated 
+        Gives as second argument a pd series with number of trades
+        """
+        #get turnover of portfolio for the day in percentage
+        weights_calc = weights+1
+        df_turnover = weights_calc.pct_change()
+        df_turnover[df_turnover!=0] = weight
+        df_turnover.iloc[0,:] = 1/len(df_turnover.columns) #sums up to one for turnover
+        #get nbr of trades for gas fees calcs
+        df_nbr_trades = df_turnover.copy()
+        df_nbr_trades[df_nbr_trades != 0] = 1
+        df_nbr_trades = df_nbr_trades.sum(axis=1)
+        #multiply it with (SLIPPAGE + LIQUID FEE) 
+        df_turnover = df_turnover.sum(axis=1)
+        df_fees = df_turnover * (SLIPPAGE_FEE + LIQUIDITY_FEE)
+        df_returns_net = returns - df_fees
+        return df_returns_net, df_nbr_trades
 
-def beta(returns, bench_returns):
-    """
-    Returns the beta of returns with the benchmark returns
-    """
-    df_cov = pd.concat([returns, bench_returns], axis=1).cov()
-    return round(df_cov.iloc[0, 1] / df_cov.iloc[0,0],4)
+    def monthly_returns(returns):
+        #get first date and find first day of next month
+        first = returns.index[0]
+        if int(first.strftime("%d")) != 1:
+            next_month = first.replace(day=27) + timedelta(days=5)
+            first = next_month - timedelta(days=(next_month.day-1))
+        #get last date and find last day of last month
+        last = returns.index[-1]
+        last = last - timedelta(days=last.day)
+        #filter df
+        returns = returns[(returns.index >= first) & (returns.index <= last)]
+        #average returns over each month
+        df = pd.DataFrame()
+        while len(returns) > 0:
+            next_month = returns.index[0].replace(day=27) + timedelta(days=5)
+            last_day = next_month - timedelta(days=next_month.day)
+            monthly_returns = returns[returns.index <= last_day] + 1
+            monthly_returns = pd.Series(monthly_returns.product() - 1, index=[last_day.strftime("%y.%m")])
+            df = pd.concat([df,monthly_returns])
+            returns = returns[returns.index > last_day]
+        df.rename(columns={0: "average_returns"}, inplace=True)
+        return df
 
-def trackingError(returns, bench_returns):
-    """
-    returns the tracking error of returns and of a benchmark returns
-    """
-    return round((returns - bench_returns).std(),4)
+    def sharpe(returns):
+        """
+        Returns the Sharpe ratio of returns given based on the earning rate of the Compound protocol
+        """
+        return round(((total_returns(returns)[0] - 1) - 0.0154) / returns.std(), 4) #0.0154 value is earnings of Compound on 01.01.23
 
-def informationRatio(returns, bench_returns):
-    """
-    Returns the information ratio of returns
-    """
-    TE = trackingError(returns, bench_returns)
-    if TE != 0:
-       return round((total_returns(returns)[0] - total_returns(bench_returns)[0]) / trackingError(returns, bench_returns))
-    else:
-        return 0
+    def beta(returns, bench_returns):
+        """
+        Returns the beta of returns with the benchmark returns
+        """
+        df_cov = pd.concat([returns, bench_returns], axis=1).cov()
+        return round(df_cov.iloc[0, 1] / df_cov.iloc[0,0],4)
 
-def bear_bull_returns(returns):
-    """
-    Separates returns between bull and bear markets and gives a tuple with first bull market returns and then bear market returns
-    Uses this analysis for dates of bull/bear markets https://crypto.com/research/crypto-bear-markets
-    """
-    #get all relevant bear markets as dateranges
-    bear_market_dates = pd.date_range(start="06.26.2019", end="03.12.2020").union(pd.date_range(start="04.14.2021", end="05.17.2021")).union(pd.date_range(start="11.10.2021", end=datetime.today()))
-    #create 2 dataframes by filtering out/in these dateranges in index
-    bear_market_dates = bear_market_dates[(bear_market_dates >= returns.index[0]) & (bear_market_dates <= returns.index[-1])]
-    bear_returns = total_returns(returns.loc[bear_market_dates])[0] - 1
-    bull_returns = total_returns(returns.drop(bear_market_dates))[0] - 1
-    return (bull_returns, bear_returns)
+    def trackingError(returns, bench_returns):
+        """
+        returns the tracking error of returns and of a benchmark returns
+        """
+        return round((returns - bench_returns).std(),4)
+
+    def informationRatio(returns, bench_returns):
+        """
+        Returns the information ratio of returns
+        """
+        TE = trackingError(returns, bench_returns)
+        if TE != 0:
+        return round((total_returns(returns)[0] - total_returns(bench_returns)[0]) / trackingError(returns, bench_returns))
+        else:
+            return 0
+
+    def bear_bull_returns(returns):
+        """
+        Separates returns between bull and bear markets and gives a tuple with first bull market returns and then bear market returns
+        Uses this analysis for dates of bull/bear markets https://crypto.com/research/crypto-bear-markets
+        """
+        #get all relevant bear markets as dateranges
+        bear_market_dates = pd.date_range(start="06.26.2019", end="03.12.2020").union(pd.date_range(start="04.14.2021", end="05.17.2021")).union(pd.date_range(start="11.10.2021", end=datetime.today()))
+        #create 2 dataframes by filtering out/in these dateranges in index
+        bear_market_dates = bear_market_dates[(bear_market_dates >= returns.index[0]) & (bear_market_dates <= returns.index[-1])]
+        bear_returns = total_returns(returns.loc[bear_market_dates])[0] - 1
+        bull_returns = total_returns(returns.drop(bear_market_dates))[0] - 1
+        return (bull_returns, bear_returns)
+
 
 
 #get the data from postgres
 df = pg.get_postgres(table_name="hist_prices", index_col="index")
 
-#data cleaning
-df.fillna(method="ffill", inplace=True) #adds missing days
-df = df.iloc[len(df)-NBR_DAYS:] #keeps only cryptos with 2 year of data
-df.dropna(axis=1, inplace=True)
+#BTC only analysis
 
-#get returns
-df_returns = df.pct_change()
-df_returns.dropna(how="all", inplace=True)
-#change returns outliers to 0
-val = df_returns.values
-og_shape = val.shape
-val = val.ravel()
-df_temp = pd.Series(val)
-df_temp[(df_temp > 1) | (df_temp < -1)] = 0
-df_temp = df_temp.values.reshape(og_shape)
-df_returns = pd.DataFrame(df_temp, columns = df_returns.columns, index=df_returns.index)
+btc = Analyzer(df)
 
 #df_gas_price
 df_gas_price = pd.read_csv("gas_price_gwei.csv") #gas price as csv from https://polygonscan.com/chart/gasprice TO BE UPDATED BY USING SELENIUM
